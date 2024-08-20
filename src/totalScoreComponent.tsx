297bbc61d6938167beac5ca3b5b54ff64b30b1ae
elviritica <e.aviles.s@gmail.com>
2024-08-20 17:55:53 +0200

    added: webpack config for dev & prod

--- src/totalScoreComponent.tsx
+++ src/totalScoreComponent.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { getTotalScore } from "./averageService";
 import * as classes from "./totalScoreComponentStyles.scss";
-import "bootstrap/dist/css/bootstrap.css";
 
 export const TotalScoreComponent = () => {
   const [totalScore, setTotalScore] = React.useState(0);
